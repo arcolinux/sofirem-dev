b364b171984af3359003a93a17682e0c61cfb8f0
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-22 14:00:01 +0100

    first commit yaml files

--- git-v1.sh
+++ git-v1.sh
@@ -33,7 +33,7 @@ git commit -m "$input"
 
 # Push the local files to github
 
-git push -u origin master
+git push -u origin main
 
 
 echo "################################################################"
